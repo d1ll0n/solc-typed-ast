7a27f88f02baf7dc36a523995a680aed35607b03
d1ll0n <dillon@indexed.finance>
2022-11-17 14:25:28 -0600

    enable algebra with yul expressions using mathjs

--- src/ast/implementation/yul/expression/yul_expression.ts
+++ src/ast/implementation/yul/expression/yul_expression.ts
@@ -1,3 +1,8 @@
+import { ExpressionAccessors, withExpressionAccessors } from "../algebra/arithmetic";
 import { YulASTNode } from "../yul_ast_node";
 
+@withExpressionAccessors
 export class YulExpression extends YulASTNode {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface YulExpression extends ExpressionAccessors {}
